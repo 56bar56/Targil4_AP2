07807c8a2e6dfb4ba7583c9e8372118c74183587
56bar56 <barbar56652>
2023-06-21 01:56:00 +0300

    fixing little problems

--- app/src/main/java/com/example/targil4_ap2/AppDB.java
+++ app/src/main/java/com/example/targil4_ap2/AppDB.java
@@ -2,8 +2,10 @@ package com.example.targil4_ap2;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 @Database(entities = {DbObject.class}, version = 1)
+@TypeConverters(DbObjectConverter.class) // Apply the converter to the database
 public abstract class AppDB extends RoomDatabase {
     public abstract PostDao postDao();
 }
---
+++ app/src/main/java/com/example/targil4_ap2/DbObjectConverter.java
@@ -0,0 +1,33 @@
+package com.example.targil4_ap2;
+
+import androidx.room.TypeConverter;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.example.targil4_ap2.items.Contact;
+import com.example.targil4_ap2.items.MessageToGet;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class DbObjectConverter {
+    @TypeConverter
+    public static List<MessageToGet> fromJson(String value) {
+        Type listType = new TypeToken<List<MessageToGet>>() {}.getType();
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static String toJson(List<MessageToGet> list) {
+        return new Gson().toJson(list);
+    }
+
+    @TypeConverter
+    public static Contact fromContactJson(String value) {
+        return new Gson().fromJson(value, Contact.class);
+    }
+
+    @TypeConverter
+    public static String toContactJson(Contact contact) {
+        return new Gson().toJson(contact);
+    }
+}
+
